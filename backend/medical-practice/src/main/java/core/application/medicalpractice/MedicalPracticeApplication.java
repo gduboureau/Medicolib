138d936dc5971bb43862f925f48f99699428a7a8
val-lst <vloustau3@gmail.com>
2023-03-04 14:53:11 +0100

    Init backend

---
+++ backend/medical-practice/src/main/java/core/application/medicalpractice/MedicalPracticeApplication.java
@@ -0,0 +1,13 @@
+package core.application.medicalpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalPracticeApplication.class, args);
+	}
+
+}
---
+++ backend/medical-practice/src/test/java/core/application/medicalpractice/MedicalPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package core.application.medicalpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
