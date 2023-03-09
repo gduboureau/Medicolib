a5d8cdd8611c6f664e2be56b0b713c9d4d123a37
val-lst <vloustau3@gmail.com>
2023-03-09 19:18:22 +0100

    Add SQL ressources (schema, data)

---
+++ backend/medical-practice/src/main/resources/data-postgres.sql
@@ -0,0 +1,8 @@
+INSERT INTO Doctors (DoctorId, Firstname, Lastname, Speciality)
+VALUES ('D001', 'John', 'Smith', 'General Practitioner');
+
+INSERT INTO Doctors (DoctorId, Firstname, Lastname, Speciality)
+VALUES ('D002', 'Emma', 'Johnson', 'Dermatology');
+
+INSERT INTO Doctors (DoctorId, Firstname, Lastname, Speciality)
+VALUES ('D003', 'William', 'Davis', 'Neurology');
---
+++ backend/medical-practice/src/main/resources/schema-postgres.sql
@@ -0,0 +1,37 @@
+DROP TABLE IF EXISTS Patients;
+CREATE TABLE Patients ( 
+    PatientId varchar(80) CONSTRAINT Patient_key PRIMARY KEY,
+    Firstname varchar(25) CONSTRAINT Firstname_null NOT NULL,
+    Lastname varchar(25) CONSTRAINT Lastname_null NOT NULL,
+    Gender char(1) CONSTRAINT Gender_check CHECK (Gender in ('M','F')),
+    Birthday date CONSTRAINT Birthday_null NOT NULL,
+    Weight float CONSTRAINT Weight_null NOT NULL,
+    Height float CONSTRAINT Height_null NOT NULL,
+    Mail varchar(320) CONSTRAINT Mail_null NOT NULL,
+    Address varchar (250) CONSTRAINT Address_null NOT NULL
+);
+
+DROP TABLE IF EXISTS Users;
+CREATE TABLE Users ( 
+    Mail varchar(320) CONSTRAINT User_key PRIMARY KEY,
+    password varchar(50) CONSTRAINT Password_null NOT NULL
+);
+
+DROP TABLE IF EXISTS Consultations;
+CREATE TABLE Consultations (
+    ConsultationId varchar(80) CONSTRAINT Consultaiton_key PRIMARY KEY,
+    PatientId varchar(80) CONSTRAINT PatientId_null NOT NULL,
+    Date date CONSTRAINT Date_null NOT NULL,
+    BeginConsultation time CONSTRAINT BeginConsultation_null NOT NULL,
+    EndConsultation time CONSTRAINT EndConsultation_null NOT NULL,
+    DoctorId varchar(80) CONSTRAINT DoctorId_null NOT NULL
+);
+
+DROP TABLE IF EXISTS Doctors;
+CREATE TABLE Doctors (
+    DoctorId varchar(80) CONSTRAINT Doctor_key PRIMARY KEY,
+    Firstname varchar(25) CONSTRAINT Firstname_null NOT NULL,
+    Lastname varchar(25) CONSTRAINT Lastname_null NOT NULL,
+    Speciality varchar(25) CONSTRAINT Speciality_check CHECK (Speciality in ('Neurology','General Practitioner', 'Dermatology', 'Dentist', 'Ophthalmology', 'Pediatrics'))
+);
+
