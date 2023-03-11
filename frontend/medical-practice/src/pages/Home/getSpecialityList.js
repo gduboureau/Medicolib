097a97ce67598e1351c16b17cad04119917f8a21
val-lst <vloustau3@gmail.com>
2023-03-11 17:23:14 +0100

    Structure front

---
+++ frontend/medical-practice/src/pages/Home/getSpecialityList.js
@@ -0,0 +1,5 @@
+function getSpecialityList(){
+    return(
+        
+    );
+}
---
+++ frontend/medical-practice/src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../../assets/HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <h1>Bienvenue chez Cabinet Médical</h1>
+      <p>Prenez vos rendez-vous médicaux rapidement et simplement juste ici.</p>
+
+      
+
+      <button className="btn-appointment">Prendre rendez-vous</button>
+    </div>
+  );
+}
+
+export default HomePage;
