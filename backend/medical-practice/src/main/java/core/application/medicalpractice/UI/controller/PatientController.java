a65b1ea2bfceb86d231ee967ca1a080403164dbc
val-lst <vloustau3@gmail.com>
2023-03-20 14:54:50 +0100

    fix bug controller

--- backend/medical-practice/src/main/java/core/application/medicalpractice/UI/controller/PatientController.java
+++ backend/medical-practice/src/main/java/core/application/medicalpractice/UI/controller/PatientController.java
@@ -9,10 +9,12 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import core.application.medicalpractice.application.MedicalPractice;
 import core.application.medicalpractice.domain.entity.Patient;
 
+@RestController
 public class PatientController {
 
     @Autowired
