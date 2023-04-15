3dff75cc349b697f607a565935ead7e515075995
Abdoul Goudoussy Diallo <abdoul-goudoussy.diallo@etu.u-bordeaux.fr>
2023-04-15 18:31:48 +0200

    version final du style editAccount

--- frontend/medical-practice/src/pages/admin/EditAccount.js
+++ frontend/medical-practice/src/pages/admin/EditAccount.js
@@ -81,100 +81,120 @@ const Edit = () => {
         <div className="InfoPatient">
             <p>Mes Coordonnées</p>
             <form onSubmit={handleSubmit} className="form-editAccount">
-                <div>
-                    <p>Sexe</p>
-                    <label className="InfoPatient-sexe">
-                        <input type="radio" id="h" name="gender" value="M" checked={data.gender === "M"}  onChange={handleChange} /> Homme
-                        <input type="radio" id="f" name="gender" value="F" checked={data.gender === "F"}  onChange={handleChange} /> Femme
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <p>Prénom</p>
-                        <input type="text" name="firstName" defaultValue={data.firstName} onChange={handleChange} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <p>Nom</p>
-                        <input type="text" name="lastName" defaultValue={data.lastName} onChange={handleChange} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <p>Date de naissance</p>
-                        <input type="date" name="date" defaultValue={data.date} onChange={handleChange} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <p>Email</p>
-                        <input type="email" name="mail" defaultValue={data.email} onChange={handleChange} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <p>Numéro de sécurité sociale</p>
-                        <input type="text" name="numSocial" defaultValue={data.numSocial} onChange={handleChange} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <p>Poids (en kg)</p>
-                        <input type="number" name="weight" defaultValue={data.weight} onChange={handleChange} />
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        <p>Taille (en cm)</p>
-                        <input type="number" name="height" defaultValue={data.height} onChange={handleChange} />
-                    </label>
-                </div>
-                <div className="InfoPatient-adress">
-                    <label className="adress-connue">
-                        <input type="radio" name="option" value="noInputs" defaultChecked onChange={handleRadioChange} />
-                        Adresse connue  
-                    </label>
-                    <label className="nouvelle-adress">
-                        <input type="radio" name="option" value="showInputs" onChange={handleRadioChange} />
-                        Nouvelle adresse 
-                    </label>
-                    {showInputs ?(
-                    <>
-                    <div>
-                        N°Rue
-                        <input type="number" name="NumRue" defaultValue={data.NumRue} onChange={handleChange} required/>
+                <div className="infoPatient-mere1">
+                    <div className="personalInformation">
+                        <h3>Mes informations personnelles</h3>
+                        <div className="info-lastName-firstName">
+                            <div>
+                                <label>
+                                    <input type="text" name="lastName" placeholder="Votre nom" defaultValue={data.lastName} onChange={handleChange} />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <input type="text" name="firstName" placeholder="Votre prénom" defaultValue={data.firstName} onChange={handleChange} />
+                                </label>
+                            </div>
+                        </div>
+                        <div className="secu-adress">
+                            <div className="info-secu">
+                                <label>
+                                    <input type="text" name="numSocial" placeholder="Votre numéro de sécurité sociale" defaultValue={data.numSocial} onChange={handleChange} />
+                                </label>
+                            </div>
+                            <div className="infoPatient-adress">
+                                <div className="adress-connue-inconnue">
+                                    <input type="radio" name="option" value="noInputs" defaultChecked onChange={handleRadioChange} />
+                                    Adresse connue  
+                                    <input type="radio" name="option" value="showInputs" onChange={handleRadioChange} />
+                                    Nouvelle adresse 
+                                </div>
+                                {showInputs ?(
+                                <>
+                                <div className="info-nouvelle-adress">
+                                    <div>
+                                        <input type="number" name="NumRue" placeholder="N°Rue" defaultValue={data.NumRue} onChange={handleChange} required/>
+                                    </div>
+                                    <div>
+                                        <input type="text" name="NomRue" placeholder="Nom de la rue" defaultValue={data.NomRue} onChange={handleChange} required/>
+                                    </div>
+                                    <div>
+                                        <input type="number" name="PostalCode" placeholder="Code postal" defaultValue={data.PostalCode} onChange={handleChange} required/>
+                                    </div>
+                                    <div>
+                                        <input type="text" name="City" placeholder="Ville" defaultValue={data.City} onChange={handleChange} required/>
+                                    </div>
+                                </div>
+                                </>
+                                ) : (
+                                <>
+                                <div className="info-adress-connue">
+                                    <input type="text" name="addr" placeholder="Votre adresse" value={data.address} onChange={handleChange} />
+                                </div>
+                                </>
+                                )}
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        Nom de la rue
-                        <input type="text" name="NomRue" defaultValue={data.NomRue} onChange={handleChange} required/>
+
+                    <div className="medicalInformation">
+                        <h3>Mes informations médicale</h3>
+                        <div className="info-medic-sexe-dateNaissance">
+                            <div className="infoPatient-medi-sexe">
+                                <label >
+                                <p>Sexe</p>
+                                    <input type="radio" id="h" name="gender" value="M" checked={data.gender === "M"}  onChange={handleChange} /> Homme
+                                    <input type="radio" id="f" name="gender" value="F" checked={data.gender === "F"}  onChange={handleChange} /> Femme
+                                </label>
+                            </div>
+                            <div className="info-medic-date">
+                                <label>
+                                    <p>Date de naissance</p>
+                                    <input type="date" name="date" defaultValue={data.date} onChange={handleChange} />
+                                </label>
+                            </div>
+                        </div>
+                        <div className="info-medic-poids-taille">
+                            <div>
+                                <label>
+                                    <p>Poids (en kg)</p>
+                                    <input type="number" name="weight" defaultValue={data.weight} onChange={handleChange} />
+                                </label>
+                            </div>
+                            <div>
+                                <label>
+                                    <p>Taille (en cm)</p>
+                                    <input type="number" name="height" defaultValue={data.height} onChange={handleChange} />
+                                </label>
+                            </div>
+                        </div>
                     </div>
-                    <div>
-                        Code postal
-                        <input type="number" name="PostalCode" defaultValue={data.PostalCode} onChange={handleChange} required/>
-                    </div>
-                    <div>
-                        Ville
-                        <input type="text" name="City" defaultValue={data.City} onChange={handleChange} required/>
-                    </div>
-                    </>
-                    ) : (
-                    <>
-                    <div>
-                        <input type="text" name="addr" value={data.address} onChange={handleChange} />
-                    </div>
-                    </>
-                    )}
+
                 </div>
-                <div>
-                    <label>
-                        <p>Nouveau mot de passe</p>
-                        <input type="password" name="password" defaultValue={data.password} onChange={handleChange} />
-                    </label>
+
+                <div className="infoPatient-mere2">
+                    <div className="myAccount">
+                        <h3>Mon compte</h3>
+                        <div>
+                            <input type="email" name="mail" placeholder="Votre adresse email" defaultValue={data.email} onChange={handleChange} />
+                        </div>
+                        <div>
+                            <input type="password" name="password" placeholder="Votre nouveau mot de passe" defaultValue={data.password} onChange={handleChange} />
+                        </div>
+                    </div>
+                    <div className="deleteAccount">
+                        <h3>Supprimer le compte</h3>
+                        <button type="submit" className="btn supprimer-compte">
+                            Supprimer
+                        </button>
+                    </div>
                 </div>
-                <button type="submit" className="btn modif-info">
-                    Enregistrer les modifications
-                </button>
+                <div className="info-registration">
+                    <button type="submit" className="btn modif-info">
+                            Enregistrer les modifications
+                    </button>
+                </div>
+
             </form>
         </div>
     );
